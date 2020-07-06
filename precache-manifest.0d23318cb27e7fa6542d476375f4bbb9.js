8b16597d8c9c2d91fd1f903b506d2ef7576228fb
Nikki Purcell <nikkiblade68@yahoo.com>
2020-07-06 16:37:54 -0700

    Updates

---
+++ precache-manifest.0d23318cb27e7fa6542d476375f4bbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f87fe68a8231d1bf4a368dff671a116",
+    "url": "/happy-garden/index.html"
+  },
+  {
+    "revision": "c6fc84419a3bde0848b0",
+    "url": "/happy-garden/static/css/main.3ea2ddda.chunk.css"
+  },
+  {
+    "revision": "ea2e29f453279ccc3d23",
+    "url": "/happy-garden/static/js/2.7c5b3ea5.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/happy-garden/static/js/2.7c5b3ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fc84419a3bde0848b0",
+    "url": "/happy-garden/static/js/main.906d9dc1.chunk.js"
+  },
+  {
+    "revision": "68f00120bd38f52da26b",
+    "url": "/happy-garden/static/js/runtime-main.df69fed3.js"
+  }
+]);
